ae5dc9478775ed91d66d4d4806f0b8f7a4037ec5
cedar <1752440588@qq.com>
2023-07-02 21:25:42 +0800

    router-demo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由模块
+import router from './router'
+import './index.css'
+
+// 导入样式
+import './assets/css/bootstrap.css'
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
